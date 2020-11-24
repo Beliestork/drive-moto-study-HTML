c187698acce7497b5c0e90421e370b8e117e1585
Ivan Bely (Benestork) <belystork@gmail.com>
2020-11-25 00:11:03 +0300

    Initial project commit. Already done <header> of the site. Now I are work on the banner section.

---
+++ js/main.js
@@ -0,0 +1,11 @@
+;(function(d){
+    'use strict';
+
+    $('.js-slick-banner').slick({
+        prevArrow: '<button class="banner__slider-arrow banner__slider-arrow--prev" type="button"></button>',
+        nextArrow: '<button class="banner__slider-arrow banner__slider-arrow--next" type="button"></button>',
+        dots: true,
+        infinite: true,
+    });
+
+})(document);
