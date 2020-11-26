e9c2393d4e91ef284a75ea8d362d04caaed85ab3
Ivan Bely (Benestork) <belystork@gmail.com>
2020-11-26 23:53:37 +0300

    End with <category-search> section.
    On branch master
    Changes to be committed:
    	modified:   css/style.css
    	modified:   index.html
    	modified:   js/main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-;(function(d){
+;(function(d) {
     'use strict';
 
     $('.js-slick-banner').slick({
@@ -7,5 +7,40 @@
         dots: true,
         infinite: true,
     });
+
+
+    /* Tab for <category-search> section */
+    const categorySearch = document.querySelector('.js-category-search');
+
+    if (categorySearch) {
+        const categoryTabs = document.querySelectorAll('.js-category-search-tab');
+        const categoryforms = document.querySelectorAll('.js-category-search-form');
+        
+        for (let i = 0; i < categoryTabs.length; i++) {
+            categoryTabs[i].addEventListener('click', handleTabClick);
+        }
+
+        function handleTabClick(e) {
+            const formId = e.target.dataset.categoryTabFor;
+            const form = document.getElementById(formId);
+            
+            if (form) {
+                const tab = e.currentTarget; 
+
+                removeSelectors(categoryTabs, 'category-search__tab--active');
+                removeSelectors(categoryforms, 'category-search__form--active');
+
+                tab.classList.add('category-search__tab--active');
+                form.classList.add('category-search__form--active');               
+            }
+        }
+
+        function removeSelectors(elements, selector) {
+            for (let i = 0; i < elements.length; i++) {
+                elements[i].classList.remove(selector);
+            }
+        }
+    }
+    /* .End */
 
 })(document);
