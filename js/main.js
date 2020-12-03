37790be6698f0120448e1b71b398b4627af630fb
Ivan Bely (Benestork) <belystork@gmail.com>
2020-12-03 21:21:08 +0300

    End with <products--popular> section. Change tab implementation.

--- js/main.js
+++ js/main.js
@@ -8,39 +8,71 @@
         infinite: true,
     });
 
+    $('.js-slick-product-popular').slick({
+        dots: false,
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 2,
+        variableWidth: true,
+        prevArrow: '<button class="products__arrow products__arrow--prev" type="button"></button>',
+        nextArrow: '<button class="products__arrow products__arrow--next" type="button"></button>',
+    });
 
-    /* Tab for <category-search> section */
-    const categorySearch = document.querySelector('.js-category-search');
+    /* Tabs */
+    const tabs = document.querySelectorAll('.js-tab');
+    const tabContents = document.querySelectorAll('.js-tab-content');
+    
+    for (let i = 0; i < tabs.length; i++) {
+        tabs[i].addEventListener('click', handleTabClick);
+    }
 
-    if (categorySearch) {
-        const categoryTabs = document.querySelectorAll('.js-category-search-tab');
-        const categoryforms = document.querySelectorAll('.js-category-search-form');
+    function handleTabClick(e) {
+        const contentId = e.target.dataset.tabFor;
+        const content = document.getElementById(contentId);
+        const isolateElement = e.target.closest('.js-tab-isolate');
         
-        for (let i = 0; i < categoryTabs.length; i++) {
-            categoryTabs[i].addEventListener('click', handleTabClick);
-        }
+        if (content) {
+            const tab = e.currentTarget; 
 
-        function handleTabClick(e) {
-            const formId = e.target.dataset.categoryTabFor;
-            const form = document.getElementById(formId);
-            
-            if (form) {
-                const tab = e.currentTarget; 
+            removeSelectors(tabs, 'js-tab--active', isolateElement);
+            removeSelectors(tabContents, 'js-tab-content--active', isolateElement);
 
-                removeSelectors(categoryTabs, 'category-search__tab--active');
-                removeSelectors(categoryforms, 'category-search__form--active');
-
-                tab.classList.add('category-search__tab--active');
-                form.classList.add('category-search__form--active');               
-            }
-        }
-
-        function removeSelectors(elements, selector) {
-            for (let i = 0; i < elements.length; i++) {
-                elements[i].classList.remove(selector);
-            }
+            tab.classList.add('js-tab--active');
+            content.classList.add('js-tab-content--active');               
         }
     }
+
+    function removeSelectors(elements, selector, isolateElement) {
+        let tmp = [];
+        
+        if (isolateElement) {
+            for (let i = 0; i < elements.length; i++) {
+                const isolate = elements[i].closest('.js-tab-isolate');
+                if (isolate === isolateElement) {
+                    tmp.push(elements[i]);
+                }
+            }
+        } else {
+            tmp = elements;
+        }
+
+        tmp.forEach(item => {
+            item.classList.remove(selector);
+        });
+    }
+    /* .End */
+
+    /* Heart toggle */
+    const toggleHeartBtn = function(e) {
+        const target = e.target;
+        const heartElement = target.closest('.heart');
+
+        if (heartElement) {
+            heartElement.classList.toggle('heart--active');
+        }
+    };  
+
+    document.addEventListener('click', toggleHeartBtn);
     /* .End */
 
 })(document);
