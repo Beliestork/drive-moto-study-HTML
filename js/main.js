9b18593b7a45238b28c2711059a222a00b584e7a
Ivan Bely (Benestork) <belystork@gmail.com>
2021-01-11 11:03:03 +0300

    End with adaptive for catalog-page

--- js/main.js
+++ js/main.js
@@ -313,4 +313,35 @@
     }
     /* .End */
 
+    /* Раскрытие фильтра на на странице категории товара */
+    const sideFilter = document.querySelector('.js-side-filter');
+    const sideFilterTrigger = document.querySelector('.js-side-filter-display-btn');
+
+    if (sideFilter && sideFilterTrigger) {
+        sideFilterTrigger.addEventListener('click', function() {
+            if (sideFilter.classList.contains('js-side-filter--active')) {
+                // Если элемент уже раскрыт
+                sideFilter.classList.remove('js-side-filter--active');
+                sideFilter.style.maxHeight = null;
+                
+                setAntonimWord(sideFilterTrigger);
+            } else {
+                // Если элемент свернут
+                sideFilter.classList.add('js-side-filter--active');
+                sideFilter.style.maxHeight = sideFilter.scrollHeight + 'px';
+                
+                setAntonimWord(sideFilterTrigger);
+            }
+
+            function setAntonimWord(elm) {
+                if (!elm.dataset.antonim) return;
+
+                var prevText = elm.textContent;
+                elm.textContent = elm.dataset.antonim;
+                elm.dataset.antonim = prevText;
+            }
+        });
+    }
+    /* .End */
+
 })(document);
